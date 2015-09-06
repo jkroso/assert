e1077582ec8683cd3a819ed0f03b3818a977e3e8
Jake Rosoman <jkroso@gmail.com>
2015-09-06 19:27:23 +1200

    simplify

--- example.js
+++ example.js
@@ -1,31 +1,4 @@
+var assert = require('./')
 
-var assert = require('./');
-
-[
-  function() {
-    var user = { name: 'Tobi' }
-    assert(user.name == 'tobi')
-  },
-  function() {
-    assert.equal(1, 2)
-  },
-  function() {
-    assert.notEqual(1, 1)
-  },
-  function() {
-    assert.throws(function() {
-      throw new Error('boom')
-    }, /Boom/)
-  },
-  function() {
-    assert.throws(function() {
-      1 + NaN
-    })
-  }
-].forEach(function(fn) {
-  try { fn() }
-  catch (e) {
-    return console.log(e.message)
-  }
-  throw new Error('you should not see this error')
-})
+var user = { name: 'Tobi' }
+assert(user.name == 'tobi')
--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-
-var src = require('function-source')
-var equal = require('equals')
-var stack = require('stack')
-var match = require('match')
-var json = JSON.stringify
+var stack = require('callsite')
 
 /**
  * Load contents of `script`.
@@ -37,7 +32,7 @@ function assert(expr, msg, depth){
 }
 
 /**
- * Create a message from the call stack.
+ * Create a message from the call stack
  *
  * @return {String}
  * @api private
@@ -53,60 +48,6 @@ function message(depth) {
   line = src.split('\n')[line]
   var m = line.slice(col).match(/assert\((.*)\)/)
   return (m ? m[1] : line).trim()
-}
-
-/**
- * deep equality test
- *
- * @param {Any} a
- * @param {Any} b
- * @param {String} [msg]
- */
-
-assert.equal = function(a, b, msg) {
-  assert(equal(a, b), msg, 1)
-}
-
-assert.notEqual = function(a, b, msg) {
-  assert(!equal(a, b), msg, 1)
-}
-
-/**
- * assert `value` matches `pattern`
- * @param {Any} a
- * @param {Any} pattern
- * @param {String} [msg]
- */
-
-assert.match = function(a, pattern, msg) {
-  assert(match(a, pattern), msg, 1)
-}
-
-assert.notMatch = function(a, pattern, msg) {
-  assert(!match(a, pattern), msg, 1)
-}
-
-/**
- * assert that `fn` throws and error matching `msg`
- *
- * @param {Function} fn
- * @param {String|Function|RegExp} [msg]
- */
-
-assert.throws = function(fn, msg) {
-  try { fn() }
-  catch (e) {
-    if (!msg) return
-    if (typeof msg == 'function') {
-      assert(e instanceof msg, 'expected an ' + msg.name + ' to be thrown')
-    } else {
-      assert(match(e.message, msg), ''
-        + 'error.message "' + e.message + '" does not match '
-        + (msg instanceof RegExp ? msg.toString() : json(msg)))
-    }
-    return
-  }
-  throw new Error('expected an error in: ' + json(src(fn).trim()))
 }
 
 module.exports = assert
